271b35a2de9235d62a29e8ebd870f0e009e38931
Guido Van Hoecke <guivho@gmail.com>
2021-01-17 20:16:16 +0100

    changed as suggested by Trevoir to solve the invalidcast error

--- BookStore-UI/Startup.cs
+++ BookStore-UI/Startup.cs
@@ -1,12 +1,15 @@
 using Blazored.LocalStorage;
 using BookStore_UI.Contracts;
+using BookStore_UI.Providers;
 using BookStore_UI.Service;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.IdentityModel.Tokens.Jwt;
+
 
 namespace BookStore_UI
 {
@@ -27,6 +30,9 @@ namespace BookStore_UI
             services.AddServerSideBlazor();
             services.AddBlazoredLocalStorage();
             services.AddHttpClient();
+            services.AddScoped<ApiAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(p =>
+                p.GetRequiredService<ApiAuthenticationStateProvider>());
             services.AddScoped<JwtSecurityTokenHandler>();
             services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
         }
