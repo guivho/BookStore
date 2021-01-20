f0def02f244d76b51ca8874439ec06bf1d4f8cae
Guido Van Hoecke <guivho@gmail.com>
2021-01-20 13:52:56 +0100

    end 53; update author working

---
+++ BookStore-UI/Static/Util.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BookStore_UI.Static
+{
+    public static class Util
+    {
+        //public static EventCallback NavigateTo(NavigationManager navigationManager, string target)
+        //{
+        //    navigationManager.NavigateTo(target);
+        //    return EventCallback();
+        //}
+    }
+}
