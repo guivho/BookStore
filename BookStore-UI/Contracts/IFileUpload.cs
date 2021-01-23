56b14c5f0dc1f0dbf8e9396f3ebcc6b49b924446
Guido Van Hoecke <guivho@gmail.com>
2021-01-23 15:18:08 +0100

    Create book (inc upload)
    Using the 0 length upload fix in the q&a  ap section 63

---
+++ BookStore-UI/Contracts/IFileUpload.cs
@@ -0,0 +1,11 @@
+﻿using BlazorInputFile;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Contracts
+{
+    public interface IFileUpload
+    {
+        public Task UploadFile(IFileListEntry file, MemoryStream memoryStream, string picName);
+    }
+}
