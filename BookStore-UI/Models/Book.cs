732df037ae98bbab9016a64934c2a19e9ed7f410
Guido Van Hoecke <guivho@gmail.com>
2021-01-23 15:35:59 +0100

    end 64, create book finished

--- BookStore-UI/Models/Book.cs
+++ BookStore-UI/Models/Book.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BookStore_UI.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public int? Year { get; set; }
+        [Required]
         public string Isbn { get; set; }
+        [StringLength(150)]
         public string Summary { get; set; }
         public string Image { get; set; }
         public decimal? Price { get; set; }
+        [Required]
         public int? AuthorId { get; set; }
         public virtual Author Author { get; set; }
     }
