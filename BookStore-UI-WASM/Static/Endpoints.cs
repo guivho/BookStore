9859afdeac2f2d053edcb50306e3fccd3416d248
Guido Van Hoecke <guivho@gmail.com>
2021-01-24 20:07:02 +0100

    Begin 70, after copy over from BookStore-UI

---
+++ BookStore-UI-WASM/Static/Endpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore_UI.Static
+{
+    public static class Endpoints
+    {
+        public static string BaseUrl = "https://localhost:44361/api/";
+        public static string AuthorsEndpoint = $"{BaseUrl}authors/";
+        public static string BooksEndpoint = $"{BaseUrl}books/";
+
+        public static string UsersUrl = $"{BaseUrl}users/";
+        public static string LoginEndpoint = $"{UsersUrl}login/";
+        public static string RegisterEndpoint = $"{UsersUrl}register/";
+    }
+}
